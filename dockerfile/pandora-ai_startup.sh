97b83040ecdb29d88af23b9e903c2dd9a99d69d8
nili <1791356563@qq.com>
2023-04-03 14:03:26 +0800

    update

--- dockerfile/pandora-ai_startup.sh
+++ dockerfile/pandora-ai_startup.sh
@@ -7,7 +7,9 @@ M_DIR="/pandora-ai"
 if [ ! -e "$M_DIR/.env" ]; then
   echo "配置文件不存在，正在生成配置文件模板，请修改配置文件并重新启动"
   cp -r $M_DIR/.env.example $M_DIR/.env
-  echo $API_BASE_URL > $M_DIR/.env
+elif [ -e "$M_DIR/.env" -a $(cat $M_DIR/.env|wc -l) -eq 0 ]; then
+  echo "配置文件已存在，但无参数，正在生成参数，请修改配置文件并重新启动"
+  cat $M_DIR/.env.example > $M_DIR/.env
 else
   echo "配置文件已存在，请修改配置文件(修改完毕请忽略)"
 fi
